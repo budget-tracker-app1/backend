b6442fbfca21145433f1103fef5a434b6210ea8d
araz <araz944memmedov@gmail.com>
2024-11-11 21:39:21 +0400

    Minor changes

--- src/main/java/com/my_sample_project/BudgetApp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/my_sample_project/BudgetApp/service/UserDetailsServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.ArrayList;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -18,11 +18,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
         return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                Collections.emptyList()
+            user.getUsername(),
+            user.getPassword(),
+            new ArrayList<>()
         );
     }
 }
