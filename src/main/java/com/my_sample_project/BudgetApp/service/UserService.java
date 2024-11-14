c447fa3d757c4b4594d2961110a3803f2383b15e
araz <araz944memmedov@gmail.com>
2024-11-14 23:13:30 +0400

    Updated several files

--- src/main/java/com/my_sample_project/BudgetApp/service/UserService.java
+++ src/main/java/com/my_sample_project/BudgetApp/service/UserService.java
@@ -1,4 +1,8 @@
 package com.my_sample_project.BudgetApp.service;
+
+import com.my_sample_project.BudgetApp.dto.api.ApiResponseDTO;
+import com.my_sample_project.BudgetApp.dto.error.ErrorResponseDTO;
+import com.my_sample_project.BudgetApp.dto.user.UserDTO;
 import com.my_sample_project.BudgetApp.model.User;
 import com.my_sample_project.BudgetApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +21,31 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public String registerUser(User user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
+    public ApiResponseDTO registerUser(UserDTO userDTO) {
+        // Check if the username already exists
+        if (userRepository.existsByUsername(userDTO.getUsername())) {
+            // Use ErrorResponseDTO for structured error response
+            return new ErrorResponseDTO(
+                "Username already taken.",
+                400,
+                "Bad Request"
+            );
+        }
 
+        // Convert DTO to entity
+        User user = new User();
+        user.setUsername(userDTO.getUsername());
+
+        // Encode the password before saving
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        // Save the user
         userRepository.save(user);
 
-        return "User registered successfully.";
+        // Use ApiResponseDTO for structured success response
+        return new ApiResponseDTO(
+            true,
+            "User registered successfully."
+        );
     }
 }
