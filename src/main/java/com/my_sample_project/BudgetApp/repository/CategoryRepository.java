9216217508702e60c9aecf27b6273a468c196950
araz <araz944memmedov@gmail.com>
2024-11-14 23:12:10 +0400

    Minor changes

--- src/main/java/com/my_sample_project/BudgetApp/repository/CategoryRepository.java
+++ src/main/java/com/my_sample_project/BudgetApp/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.my_sample_project.BudgetApp.repository;
 
+import com.my_sample_project.BudgetApp.dto.category.CategoryProjection;
 import com.my_sample_project.BudgetApp.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    List<Category> findByUserId(Long userId);
+    List<CategoryProjection> findByUserId(Long userId);
 }
