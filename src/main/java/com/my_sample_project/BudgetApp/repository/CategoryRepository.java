142d354c62e610e970a9bf71617ceb546d5e7c3c
araz <araz944memmedov@gmail.com>
2024-11-30 23:58:29 +0400

    Added findByNameAndTypeAndUserId() in repository file

--- src/main/java/com/my_sample_project/BudgetApp/repository/CategoryRepository.java
+++ src/main/java/com/my_sample_project/BudgetApp/repository/CategoryRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     List<CategoryProjection> findByUserId(Long userId);
     boolean existsByNameAndTypeAndUserId(String name, CategoryType type, Long userId);
+    Optional<Category> findByNameAndTypeAndUserId(String name, CategoryType type, Long userId);
 }
