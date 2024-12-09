40cd075ec9a096766947f24b934b63348eac46d6
araz <araz944memmedov@gmail.com>
2024-12-09 12:14:36 +0400

    Added @JsonProperty annotation

--- src/main/java/com/my_sample_project/BudgetApp/dto/error/ErrorResponseDTO.java
+++ src/main/java/com/my_sample_project/BudgetApp/dto/error/ErrorResponseDTO.java
@@ -1,22 +1,33 @@
 package com.my_sample_project.BudgetApp.dto.error;
 
 import com.my_sample_project.BudgetApp.dto.api.ApiResponseDTO;
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ErrorResponseDTO extends ApiResponseDTO {
     private int statusCode;
     private String error;
+    private LocalDateTime timestamp;
 
     public ErrorResponseDTO(String message, int statusCode, String error) {
         super(false, message);
         this.statusCode = statusCode;
         this.error = error;
+        this.timestamp = LocalDateTime.now(); // Current timestamp
     }
 
+    @JsonProperty
     public int getStatusCode() {
         return statusCode;
     }
 
+    @JsonProperty
     public String getError() {
         return error;
+    }
+
+    @JsonProperty
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 }
