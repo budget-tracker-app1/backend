790d96bd570e5a31fddaa6724214dc7681765624
araz <araz944memmedov@gmail.com>
2024-12-09 12:08:42 +0400

    Minor changes

--- src/main/java/com/my_sample_project/BudgetApp/BudgetAppApplication.java
+++ src/main/java/com/my_sample_project/BudgetApp/BudgetAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.my_sample_project.BudgetApp")
 @EnableJpaRepositories("com.my_sample_project.BudgetApp.repository")
 public class BudgetAppApplication {
 	public static void main(String[] args) {
