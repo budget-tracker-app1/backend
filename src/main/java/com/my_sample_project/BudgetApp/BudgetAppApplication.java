1abf576d01bd85db96879bba5a1c0f1fc205b0a2
araz <araz944memmedov@gmail.com>
2024-12-30 20:42:39 +0400

    Update

--- src/main/java/com/my_sample_project/BudgetApp/BudgetAppApplication.java
+++ src/main/java/com/my_sample_project/BudgetApp/BudgetAppApplication.java
@@ -1,10 +1,7 @@
 package com.my_sample_project.BudgetApp;
 
-import com.my_sample_project.BudgetApp.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.my_sample_project.BudgetApp")
@@ -12,16 +9,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BudgetAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetAppApplication.class, args);
-	}
-
-	// Load .env values into the Spring Environment
-	public static class EnvLoader implements EnvironmentPostProcessor {
-		@Override
-		public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-			System.setProperty("DB_URL", EnvConfig.get("DB_URL"));
-			System.setProperty("DB_USERNAME", EnvConfig.get("DB_USERNAME"));
-			System.setProperty("DB_PASSWORD", EnvConfig.get("DB_PASSWORD"));
-			System.setProperty("JWT_SECRET", EnvConfig.get("JWT_SECRET"));
-		}
 	}
 }
