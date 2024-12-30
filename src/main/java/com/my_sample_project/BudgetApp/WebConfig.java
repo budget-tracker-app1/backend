4ba74da7ca35173489f15fe1c0bf8fff43042fa4
araz <araz944memmedov@gmail.com>
2024-12-30 22:46:08 +0400

    Update

--- src/main/java/com/my_sample_project/BudgetApp/WebConfig.java
+++ src/main/java/com/my_sample_project/BudgetApp/WebConfig.java
@@ -1,19 +1,19 @@
-package com.my_sample_project.BudgetApp;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    // Enable CORS for frontend API calls
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply CORS to all backend API endpoints
-                .allowedOrigins("https://frontend-gjap.onrender.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//package com.my_sample_project.BudgetApp;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    // Enable CORS for frontend API calls
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Apply CORS to all backend API endpoints
+//                .allowedOrigins("https://frontend-gjap.onrender.com")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
